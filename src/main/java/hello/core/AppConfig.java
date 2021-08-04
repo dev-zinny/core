fc066af126ceb9c9dede1b3f156249423cd14ace
dev-zinny <dev.zinny@gmail.com>
2021-08-04 23:33:29 +0900

    AppConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,16 +10,25 @@ import hello.core.order.OrderServiceImpl;
 
 // 어플리케이션 전체를 설정하고 구성
 // 어플리케이션 실제 동작에 필요한 구현객체를 생성
+
+// 리팩토링 :: 역할이 드러나게 변경
 public class AppConfig {
-    
-    
+
     // 생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
